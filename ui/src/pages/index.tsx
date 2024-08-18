516f43a5f411efddad79303de64798e17b13ad9d
lifeparticle <mahbubzaman17@gmail.com>
2024-08-19 00:43:29 +1000

    update

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -6,14 +6,12 @@ import { Button } from "@nextui-org/react";
 export default function IndexPage() {
 	return (
 		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-6 py-12 md:py-16">
+			<section className="flex flex-col items-center justify-center gap-6 md:py-8">
 				<div className="flex flex-col items-center justify-center text-center w-full px-4 md:px-10 lg:px-20">
 					<h1 className={`${title()} mb-2`} style={{ fontSize: "1.5rem" }}>
 						Compare <span className={title({ color: "violet" })}>GitHub</span>{" "}
-						Repositories
-					</h1>
-					<h1 className={title()} style={{ fontSize: "1.5rem" }}>
-						Based on the Number of Stars and Other Relevant Parameters
+						repositories based on the number of stars and other relevant
+						parameters.
 					</h1>
 				</div>
 
