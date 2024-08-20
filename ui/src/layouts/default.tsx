3418a991d571d231c45b870980808a51be729275
lifeparticle <mahbubzaman17@gmail.com>
2024-08-20 18:53:11 +1000

    update

--- ui/src/layouts/default.tsx
+++ ui/src/layouts/default.tsx
@@ -1,5 +1,3 @@
-import { Link } from "@nextui-org/link";
-
 import { Navbar } from "@/components/navbar";
 
 export default function DefaultLayout({
@@ -13,14 +11,6 @@ export default function DefaultLayout({
 			<main className="container mx-auto max-w-7xl px-6 flex-grow">
 				{children}
 			</main>
-			<footer className="w-full flex items-center justify-center py-3">
-				<Link
-					isExternal
-					className="flex items-center gap-1 text-current"
-					href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
-					title="nextui.org homepage"
-				></Link>
-			</footer>
 		</div>
 	);
 }
